1d27081cc760f58680ebf58ea840d0dded4c5b5f
luvega-rj <luvegaronny@gmail.com>
2023-06-27 12:45:29 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,21 +1,16 @@
 from flask import Flask
-
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
-
 @app.route('/print/<string:param>')
 def print_string(param):
     print(param)
     return param
-
 @app.route('/count/<int:param>')
 def count(param):
     numbers = '\n'.join(str(num) for num in range(param + 1))
     return numbers + '\n'
-
 @app.route('/math/<int:num1>/<operation>/<int:num2>')
 def math(num1, operation, num2):
     result = 0
